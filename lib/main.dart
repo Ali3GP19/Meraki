7302033e2a17c166dc48cad640f20ece073967c2
AlissonGP <agarciapa19@gmail.com>
2024-06-22 06:00:59 -0500

    Agregar modificaciones d2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
@@ -8,7 +9,9 @@ import 'screens/favorites_screen.dart';
 import 'screens/historial_cliente_screen.dart';
 import 'screens/perfil_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
